ffc0f9a33b65ff8570f96d651b28dff8aa7922db
fab <59584067+FabFromTheSnow@users.noreply.github.com>
2023-01-28 20:19:54 +0000

    Add files via upload

---
+++ CTFeasyrec.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# CTF recon
+echo Enter CTF Name
+read CTF
+echo Enter CTF target IP
+read target
+mkdir $CTF
+cd $CTF
+curl $target/robots.txt >> Recon.txt
+nmap -sV -Pn -T5 $target >> Recon.txt
+echo "Nmap done"
+echo "Nikto wil take time, start browsing"
+nikto -h $target >> Recon.txt
+echo check your new folder
