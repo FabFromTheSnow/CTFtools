7bc0750a765135bc738d092453eb63a65f2ed3e2
fab <59584067+FabFromTheSnow@users.noreply.github.com>
2023-01-29 20:51:46 +0000

    Update CTFeasyrec.sh
    ADDED quotation mark around $CTF to avoid multiple folder when spaces in the name

--- CTFeasyrec.sh
+++ CTFeasyrec.sh
@@ -4,8 +4,8 @@ echo Enter CTF Name
 read CTF
 echo Enter CTF target IP
 read target
-mkdir $CTF
-cd $CTF
+mkdir "$CTF"
+cd "$CTF"
 curl $target/robots.txt >> Recon.txt
 nmap -sV -Pn -T5 $target >> Recon.txt
 echo "Nmap done"
