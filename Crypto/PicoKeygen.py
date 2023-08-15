da430c85caeecb395b7bbf790cfbf0a64623bcbb
fab <59584067+FabFromTheSnow@users.noreply.github.com>
2023-08-15 15:39:21 -0400

    Update PicoKeygen.py

--- Crypto/PicoKeygen.py
+++ Crypto/PicoKeygen.py
@@ -1,22 +1,12 @@
 import hashlib
-from cryptography.fernet import Fernet
-import base64
 
-username_trial = "GOUGH"
-bUsername_trial = b"GOUGH"
-i = 0
-print((hashlib.sha256(username_trial).hexdigest()[4])
-i += 1
-print((username_trial).hexdigest()[5])
-i += 1
-print((username_trial).hexdigest()[3])
-i += 1
-print((username_trial).hexdigest()[6])
-i += 1
-print((username_trial).hexdigest()[2])
-i += 1
-print((username_trial).hexdigest()[7])
-i += 1
-print((username_trial).hexdigest()[1])
-i += 1
-print((username_trial).hexdigest()[8])
+key_part_static1_trial = "picoCTF{1n_7h3_|<3y_of_"
+key_part_static2_trial = "}"
+
+oui = "GOUGH"
+a = hashlib.sha256(oui.encode()).hexdigest()
+
+key_part_dynamic1_trial = a[4] + a[5] + a[3] + a[6] + a[2] + a[7] + a[1] + a[8]
+
+flag = key_part_static1_trial + key_part_dynamic1_trial + key_part_static2_trial
+print(flag)
