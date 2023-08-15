d71211bd7e770fc4867385d66e01925298c18e18
fab <59584067+FabFromTheSnow@users.noreply.github.com>
2023-08-15 15:17:44 -0400

    Create PicoKeygen.py

---
+++ Crypto/PicoKeygen.py
@@ -0,0 +1,22 @@
+import hashlib
+from cryptography.fernet import Fernet
+import base64
+
+username_trial = "GOUGH"
+bUsername_trial = b"GOUGH"
+
+print(hashlib.sha256(username_trial).hexdigest()[4]
+i += 1
+print(username_trial).hexdigest()[5]
+i += 1
+print(username_trial).hexdigest()[3]
+i += 1
+print(username_trial).hexdigest()[6]
+i += 1
+print(username_trial).hexdigest()[2]
+i += 1
+print(username_trial).hexdigest()[7]
+i += 1
+print(username_trial).hexdigest()[1]
+i += 1
+print(username_trial).hexdigest()[8]
