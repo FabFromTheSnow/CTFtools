46b98d0da7a3038feaca248c716823a990b760ed
fab <59584067+FabFromTheSnow@users.noreply.github.com>
2023-08-24 17:48:23 -0400

    Update CTFeasyrec.sh
    update nmap command
    automatic adding of ip to host with CTF-NAME.HTB
    Switched to vscode for file opening

--- CTFeasyrec.sh
+++ CTFeasyrec.sh
@@ -4,16 +4,32 @@ echo Enter CTF Name
 read CTF
 echo Enter CTF target IP
 read target
-export ip=$target
+echo $TARGET $CTF'.htb' >> /etc/hosts
+export ip=$TARGET
+
 mkdir "$CTF"
 cd "$CTF"
-curl $target/robots.txt --max-time 5 >> Recon.txt
-nmap -sC -sV -Pn -T5 $target >> Recon.txt
-echo "Nmap done"
-echo "open info with sublime text"
-subl Recon.txt
-echo "Nikto wil take time, saving in Nikto.txt"
-nikto -h $target >> Nikto.txt
-echo open nikto with sublime text
-subl Nikto.txt
 
+PORTS=$(nmap -sS -Pn -p- $TARGET | grep -oP '\d+(?=/tcp)' | tr '\n' ',' | sed 's/,$//')
+nmap -sC -sV -Pn $TARGET -p $PORTS >> Recon.txt
+echo "Nmap done"
+
+
+echo "open info with VScode"
+code Recon.txt
+
+echo "Nikto wil take time, saving in Nikto.txt"
+nikto -h $TARGET >> Nikto.txt
+
+echo open nikto with VScode
+code Nikto.txt
+
+
+HTTP=$(curl $TARGET --max-time 5)
+if [[ $HTTP == *"http"* ]]; then
+    # Faites quelque chose si la variable contient "http"
+    echo "La variable \$HTTP contient 'http'."
+else
+    # Passez à la suite du script si la variable ne contient pas "http"
+    echo "La variable \$HTTP ne contient pas 'http'."
+fi
