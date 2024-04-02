5635f3210c898ec3d04eaa1e087a03bbd0c6b44b
fab <fab@fabsnow.com>
2024-04-02 20:27:41 +0200

    v3

--- Rec/EasyrecEXEGOL.sh
+++ Rec/EasyrecEXEGOL.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 # CTF recon
 
-echo Enter CTF Name
+BOLD='\033[1m'
+NO_COLOR='\033[0m'
+
+echo -e "${BOLD}Enter CTF Name${NO_COLOR}"
 read CTF
-echo Enter CTF target IP
+echo -e "${BOLD}Enter CTF target IP${NO_COLOR}"
 read TARGET
-echo $TARGET $CTF'.htb' >> /etc/hosts
+CTFurl=$CTF'.htb'
+echo $TARGET $CTFurl >> /etc/hosts
 export ip=$TARGET
 
 mkdir "$CTF"
-cd "$CTF"
+cd "$CTF" || exit
 
 # Installs Visual Studio Code if not present
 if ! which code > /dev/null; then
@@ -19,23 +23,22 @@ if ! which code > /dev/null; then
   rm -f packages.microsoft.gpg
   sudo apt update
   sudo apt install code -y
-
-  
 fi
 
-echo 'start quick nmap'
-PORTS=$(nmap -sS -Pn --open -p 80,443,22,5000 $TARGET | grep -oP '\d+(?=/tcp)' | tr '\n' ',' | sed 's/,$//')
-echo 'OPEN ports :'$PORTS
-echo "starting FULL scan in background, vscode will open once done"
-nmap -sC -sV -Pn $TARGET -p $PORTS >> "Recon.txt" && code "./Recon.txt"
-echo "Masscan running in background for udp"
-(masscan -pU:0-65535 $TARGET --rate=10000 --interface tun0 >> ./Recon.txt) &
+echo -e "${BOLD}start quick nmap${NO_COLOR}"
+PORTS=$(nmap -sS -Pn --open -p 80,443,22,5000 "$TARGET" | grep -oP '\d+(?=/tcp)' | tr '\n' ',' | sed 's/,$//')
+echo -e "${BOLD}OPEN ports: $PORTS${NO_COLOR}"
+echo -e "${BOLD}starting FULL scan, vscode will open once done${NO_COLOR}"
+nmap -sC -sV -Pn "$TARGET" -p "$PORTS" >> "Recon.txt" 
+echo -e "${BOLD}Masscan running in background for udp${NO_COLOR}"
 
-if grep -qi 'http' "Recon.txt"; then
-echo 'webserver detected'
-
+if grep -qi '443/tcp open' "Recon.txt"; then
+    echo -e "${BOLD}Web server detected (https)${NO_COLOR}"
+    nohup dirb "https://$CTFurl/" /usr/share/seclists/Discovery/Web-Content/big.txt -o ./WebServ &>/dev/null &
+elif grep -qi '80/tcp open' "Recon.txt"; then
+    echo -e "${BOLD}Web server detected (http)${NO_COLOR}"
+    nohup dirb "http://$CTFurl/" /usr/share/seclists/Discovery/Web-Content/big.txt -o ./WebServ &>/dev/null &
 fi
 
-
-
-
+masscan -pU:0-65535 "$TARGET" --rate=10000 --interface tun0 >> ./Recon.txt
+code ./Recon.txt --no-sandbox --user-data-dir=./
