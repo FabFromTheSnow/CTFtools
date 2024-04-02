d5ce7e5f13054bfd5b589f68eebd1c316f99f465
Fab Snow <Fab@mail.com>
2024-04-02 14:26:11 +0200

    test V3

--- Rec/EasyrecEXEGOL.sh
+++ Rec/EasyrecEXEGOL.sh
@@ -11,13 +11,16 @@ export ip=$TARGET
 mkdir "$CTF"
 cd "$CTF"
 
+# Installs Visual Studio Code if not present
 if ! which code > /dev/null; then
   wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
   sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
   sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
   rm -f packages.microsoft.gpg
   sudo apt update
-  # Installs Visual Studio Code if not present
+  sudo apt install code
+
+  
 fi
 
 
