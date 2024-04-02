6561b2319de9dbce898d264528f6fffc476ba8b4
Fab Snow <Fab@mail.com>
2024-04-02 14:32:46 +0200

    auto accept install

--- Rec/EasyrecEXEGOL.sh
+++ Rec/EasyrecEXEGOL.sh
@@ -18,7 +18,7 @@ if ! which code > /dev/null; then
   sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
   rm -f packages.microsoft.gpg
   sudo apt update
-  sudo apt install code
+  sudo apt install code -y
 
   
 fi
