d69113ac966b04c2fe135f250b784ff858f23cce
Fab Snow <Fab@mail.com>
2024-04-02 14:11:06 +0200

    version 2

--- Rec/EasyrecEXEGOL.sh
+++ Rec/EasyrecEXEGOL.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # CTF recon
-# test
+
 echo Enter CTF Name
 read CTF
 echo Enter CTF target IP
@@ -11,7 +11,19 @@ export ip=$TARGET
 mkdir "$CTF"
 cd "$CTF"
 
+if ! which code > /dev/null; then
+  wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+  sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+  sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+  rm -f packages.microsoft.gpg
+  sudo apt update
+  # Installs Visual Studio Code if not present
+fi
+
+
 PORTS=$(nmap -sS -Pn -p- $TARGET | grep -oP '\d+(?=/tcp)' | tr '\n' ',' | sed 's/,$//')
-echo $PORTS
-nmap -sC -sV -Pn $TARGET -p $PORTS >> Recon.txt
+echo 'OPEN ports :'$PORTS
+echo "starting FULL scan in background"
+nmap -sC -sV -Pn $TARGET -p $PORTS >> Recon.txt &
 echo "Nmap done"
+
